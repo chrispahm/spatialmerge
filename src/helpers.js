1ea30deab1e96e3f3d1d15cd9a24076e43d8c251
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2022-07-25 16:00:10 +0200

    💄 rename object1 and object2 to left- and rightFeatureProps for better readability

--- src/helpers.js
+++ src/helpers.js
@@ -14,29 +14,29 @@ function findAllDuplicates(array) {
   return [...duplicates];
 }
 
-function mergeWith(object1, object2, lsuffix = 'left', rsuffix = 'right') {
-  const keys1 = Object.keys(object1)
-  const keys2 = Object.keys(object2)
+function mergeWith(leftFeatureProps, rightFeatureProps, lsuffix = 'left', rsuffix = 'right') {  
+  const keys1 = Object.keys(leftFeatureProps)
+  const keys2 = Object.keys(rightFeatureProps)
   const allKeys = [...keys1, ...keys2]
   const duplicates = findAllDuplicates(allKeys)
   const feature = {}
   if (duplicates.length === 0) {
-    keys1.forEach(key => (feature[key] = object1[key]))
-    keys2.forEach(key => (feature[key] = object2[key]))
+    keys1.forEach(key => (feature[key] = leftFeatureProps[key]))
+    keys2.forEach(key => (feature[key] = rightFeatureProps[key]))
     return feature
   } else {
     keys1.forEach(key => {
       if (duplicates.indexOf(key) > -1) {
-        feature[key + '_' + lsuffix] = object1[key]
+        feature[key + '_' + lsuffix] = leftFeatureProps[key]
       } else {
-        feature[key] = object1[key]
+        feature[key] = leftFeatureProps[key]
       }
     })
     keys2.forEach(key => {
       if (duplicates.indexOf(key) > -1) {
-        feature[key + '_' + rsuffix] = object2[key]
+        feature[key + '_' + rsuffix] = rightFeatureProps[key]
       } else {
-        feature[key] = object2[key]
+        feature[key] = rightFeatureProps[key]
       }
     })
     return feature
